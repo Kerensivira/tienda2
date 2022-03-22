27cd274dbc59caf3489176f2978c061b83020019
Alexandra Sivira <kerensivira@gmail.com>
2022-03-22 07:14:37 -0500

    jhg

--- cards.js
+++ cards.js
@@ -33,9 +33,29 @@ async function showCards() {
              <h5 class="card-title">${product}</h5>
              <h6 class="card-title">${precio}</h6>
              <p class="card-text">${unidad_kilo}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
-           </div>
-         </div>
+             
+             <div class="modal" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+                
+                </div>
+              </div>
+            </div>
+        
    </div>`
     
  });
